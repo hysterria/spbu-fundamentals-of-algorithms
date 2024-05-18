6c41854c2c9989479a65b82f1e524cb5c47faf94
Hysteria <prirychaudrakonov@gmail.com>
2024-05-18 15:15:21 +0300

    merge branchs

--- practicum_2/homework/basic/cycles_in_undirected_graph.py
+++ practicum_2/homework/basic/cycles_in_undirected_graph.py
@@ -9,12 +9,45 @@ TEST_GRAPH_FILES = [
 ]
 
 
-def has_cycles(g: nx.Graph):
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+import os
 
-    pass
+import networkx as nx
+
+
+TEST_GRAPH_FILES = [
+    "graph_1_wo_cycles.edgelist",
+    "graph_2_w_cycles.edgelist",
+]
+
+
+def has_cycles(g: nx.Graph):
+    def dfs(node, parent):
+        visited.append(node)
+        stack.append(node)
+
+        for neighbor in g[node]:
+            if neighbor == parent:
+                continue
+
+            if neighbor not in visited:
+                if dfs(neighbor, node):
+                    return True
+            elif neighbor in stack:
+                return True
+
+        visited.remove(node)
+        stack.pop()
+
+        return False
+    visited = []
+    stack = []
+
+    for node in g:
+        if node not in visited:
+            if dfs(node, None):
+                return True
+
+    return False
 
 
 if __name__ == "__main__":
